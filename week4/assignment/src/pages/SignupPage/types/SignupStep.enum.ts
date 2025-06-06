e225c99a91b6fe5cb697e2e5cc106d9a456cb5cb
Kyoungho Eom <kheom0512@gmail.com>
2025-06-06 23:40:24 +0900

    refactor: 회원가입 단계 enum 타입 상수로 변경

--- week4/assignment/src/pages/SignupPage/types/SignupStep.enum.ts
+++ week4/assignment/src/pages/SignupPage/types/SignupStep.enum.ts
@@ -1,5 +1,7 @@
-export enum SignupStep {
-  EnterId = 0,
-  EnterPassword = 1,
-  EnterNickname = 2,
-} 
+export const SignupStep = {
+  EnterId: 0,
+  EnterPassword: 1,
+  EnterNickname: 2,
+} as const;
+
+export type SignupStep = typeof SignupStep[keyof typeof SignupStep]; 
